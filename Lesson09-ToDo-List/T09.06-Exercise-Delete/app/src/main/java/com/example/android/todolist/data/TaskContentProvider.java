624639d5c5c6610593fd75fa17a280c736006912
Matteo Temperoni <metiusm88@hotmail.it>
2018-01-28 17:39:03 +0100

    COMPLETED T09.06

--- Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.06-Exercise-Delete/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -156,14 +156,33 @@ public class TaskContentProvider extends ContentProvider {
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
 
-        // TODO (1) Get access to the database and write URI matching code to recognize a single item
+        int result;
 
-        // TODO (2) Write the code to delete a single row of data
+        // COMPLETED (1) Get access to the database and write URI matching code to recognize a single item
+        final SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
+
+        int match = sUriMatcher.match(uri);
+
+        // COMPLETED (2) Write the code to delete a single row of data
         // [Hint] Use selections to delete an item by its row ID
+        switch (match) {
+            case TASK_WITH_ID:
 
-        // TODO (3) Notify the resolver of a change and return the number of items deleted
+                String id = uri.getLastPathSegment();
 
-        throw new UnsupportedOperationException("Not yet implemented");
+                result = db.delete(TABLE_NAME, "_id=?", new String[]{id});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (result > 0)
+            // COMPLETED (3) Notify the resolver of a change and return the number of items deleted
+            getContext().getContentResolver().notifyChange(uri, null);
+
+        return result;
+
+        //throw new UnsupportedOperationException("Not yet implemented");
     }
 
 
