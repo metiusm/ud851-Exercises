c714db744f19ba4dbb53ae9489660d99907ad662
Matteo Temperoni <metiusm88@hotmail.it>
2018-01-28 11:13:38 +0100

    COMPLETED T09.05

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -23,6 +23,7 @@ import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
@@ -120,15 +121,31 @@ public class TaskContentProvider extends ContentProvider {
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
 
-        // TODO (1) Get access to underlying database (read-only for query)
+        Cursor result;
+        // COMPLATED (1) Get access to underlying database (read-only for query)
+        final SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
+        // COMPLATED (2) Write URI match code and set a variable to return a Cursor
+        int match = sUriMatcher.match(uri);
+        // COMPLATED (3) Query for the tasks directory and write a default case
+        switch(match)
+        {
+            case TASKS:
+                result = db.query(TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        // COMPLATED (4) Set a notification URI on the Cursor and return that Cursor
+        result.setNotificationUri(getContext().getContentResolver(), uri);
 
-        // TODO (2) Write URI match code and set a variable to return a Cursor
-
-        // TODO (3) Query for the tasks directory and write a default case
-
-        // TODO (4) Set a notification URI on the Cursor and return that Cursor
-
-        throw new UnsupportedOperationException("Not yet implemented");
+        return result;
+        //throw new UnsupportedOperationException("Not yet implemented");
     }
 
 
